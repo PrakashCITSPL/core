f97486cd4741273b3e869ebc4f34af261eac4ff7
Alex Murzina <alexandra.murzina@solvexia.com>
2019-07-30 10:40:22 +1000

    Added a special check to the processObject method to make sure it does not process constructor methods which is an issue

---
+++ CHANGELOG.md
@@ -0,0 +1,10 @@
+### Core 0.1.9
+
+- Added a special check to the processObject method to make sure it does not process constructor methods which is an issue 
+for some cases when Javascript precompilers which add constructor as a enumerable property to classes.
+
+- Added a licence file (MIT)
+
+### Core 0.1.8
+
+- Added support for @types for Typescript.
--- index.d.ts
+++ index.d.ts
@@ -4,12 +4,12 @@ interface Core {
   FireEvent  (Request: CoreRequest, success?: Function, fail?: Function) :void;
   FireRequest(Event  : CoreEvent) :void;
 
-  CatchEvent(...args: any)  : CoreRequest;
+  CatchEvent  (...args: any): CoreEvent;
   CatchRequest(...args: any): CoreRequest;
 
   detachObject (Object: Object): Object;
   processObject(Object: Object): Object;
 }
 
-declare type CoreRequest = { _request: string } & {[key: string]: any};
-declare type CoreEvent   = { _event  : string } & {[key: string]: any};
+declare interface CoreRequest { _request: string, [key: string]: any}
+declare interface CoreEvent   { _event  : string, [key: string]: any}
