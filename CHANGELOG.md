360e9ece9116c2215fa3e5f94d2838736ca33fff
myfoxtail <{ID}+{username}@users.noreply.github.com>
2020-09-17 11:15:36 +1000

    enhanced typescript support to make it easier to create type definitions for Core Events and Requests

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+# Changelog
+
+### Core 0.1.10
+
+- Enhanced typescript support to make it easier to create type definitions for Core Events and Requests.
+
 ### Core 0.1.9
 
 - Added a special check to the processObject method to make sure it does not process constructor methods which is an issue 
--- index.d.ts
+++ index.d.ts
@@ -1,15 +1,18 @@
 declare module "core-os";
 
 interface Core {
-  FireEvent  (Request: CoreRequest, success?: Function, fail?: Function) :void;
-  FireRequest(Event  : CoreEvent) :void;
+  FireEvent  (Request: CoreRequest<string>, success?: Function, fail?: Function) :void;
+  FireRequest(Event  : CoreEvent<string>) :void;
 
-  CatchEvent  (...args: any): CoreEvent;
-  CatchRequest(...args: any): CoreRequest;
+  CatchEvent  (...args: any): CoreEvent<string>;
+  CatchRequest(...args: any): CoreRequest<string>;
 
   detachObject (Object: Object): Object;
   processObject(Object: Object): Object;
 }
 
-declare interface CoreRequest { _request: string, [key: string]: any}
-declare interface CoreEvent   { _event  : string, [key: string]: any}
+declare interface CoreRequest<request> { _request: request, [key: string]: any}
+declare interface CoreRequestConstructor<request> { new (parameters?: any): CoreRequest<request>; }
+
+declare interface CoreEvent<event>   { _event  : event, [key: string]: any}
+declare interface CoreEventConstructor<event> { new (parameters?: any): CoreEvent<event>; }
